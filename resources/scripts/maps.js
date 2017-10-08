0fcc0aa13deb439d6c33d8085c48041d9af5b260
Pawel Maricnkowski <wafel34@gmail.com>
2017-10-08 19:12:51 +0100

    added google maps

---
+++ resources/scripts/maps.js
@@ -0,0 +1,210 @@
+var MAPS = {
+    map: {},
+    service: {},
+    infoWindow: {},
+    distance: {},
+    cords: {lat: 51.524813, lng: -0.125688},
+    placesList: document.getElementById('places')
+};
+
+//INITIALIZE THE MAP (FIRED ON THE START AND ON EVERY CHANGE OF DATA (FROM SLECT INPUT: TYPE AND SORT))
+MAPS.initialize = function(config){
+
+    //CLEAR THE LIST OF PLACES BEFORE INITIALIZING
+    this.placesList.innerHTML = '';
+
+    //REQ OBJ: TAKES RANK, TYPE FROM CONFIG ARGUMENT PASSED TO THIS FUNCTION AND SETS DATA DEPENDING ON THAT
+    var request = {
+            location: this.cords,
+            type: [config.type],
+            rankBy: (config.sort === 'rank') ? google.maps.places.RankBy.RATING : google.maps.places.RankBy.DISTANCE,
+            radius: (config.sort === 'rank') ? '1000' : null
+        };
+
+    //CREATE NEW GOOGLE MAP
+    this.map = new google.maps.Map(document.getElementById('map-display'),{
+        center: this.cords,
+        zoom: 15,
+        cords: this.cords,
+        disableDefaultUI: true,
+        gestureHandling: 'cooperative'
+    });
+
+    //CREATE MARKER (ICON) FOR CENTER POINT
+    var marker = new google.maps.Marker({
+        map: this.map,
+        position: this.cords
+    });
+
+    //INITIALIZE OTHER GOOGLE SERVICES
+    this.infoWindow = new google.maps.InfoWindow();
+    this.service = new google.maps.places.PlacesService(this.map);
+    this.distance = new google.maps.DistanceMatrixService();
+
+    //INITIALIZE nearbySearch TO FIND PLACES IN LOCAL AREA AND CALL callback function
+    this.service.nearbySearch(request, this.callback);
+
+};
+
+//SEARCH FOR DATA BASING ON REQUEST OBJ FROM ABOVE AND CREATE MARKERS ON MAP FOR PLACES
+MAPS.callback = function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i=0; i<results.length; i++) {
+            MAPS.createMarker(results[i]);
+        }
+    }
+};
+
+//ACTUAL FUNCTION RESPONSIBLE FOR CREATING MARKERS
+//IT SETS THE MARKER FOR EACH PLACE FOUND IN callback FUNCTION ABOVE
+//AND CALLS 'CALCULATE DISTANCE/DISPLAY LIST' FUNCTION WHICH WILL SHOW THE LIST
+MAPS.createMarker = function(place) {
+
+    //CREATE NEW MARKER
+    var marker = new google.maps.Marker({
+        map: this.map,
+        position: place.geometry.location,
+        icon: {
+            url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png'
+        }
+    });
+
+    //ADD CLICK LISTENER ON ACTUAL MARKER
+    google.maps.event.addListener(marker, 'click', function(){
+        this.service.getDetails(place, function(result, status){
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+            }
+
+            //SET CONTENT OF INFOWINDOW THAT WILL OPEN AFTER MARKER IS CLICKED
+            this.infoWindow.setContent('<h2>'+result.name+'</h2>' + result.adr_address );
+            this.infoWindow.open(this.map, marker);
+        }.bind(this));
+    }.bind(this));
+
+    //CALL FUNCTION THAT WILL CALCULATE THE DISTANCE
+    MAPS.calculateDistance(place, marker);
+
+};
+
+//THIS FUNCTION CALCULATES THE DISTANCE BETWEEN TWO POINTS: MAIN AND EACH PACE FOUND IN callback FUNCTION ABOVE
+//IT TAKSE place AND marker AS PARAMETERS. MARKER IS NEEDED TO LINK THE LIST WITH ACTUAL MARKER ON THE MAP LATER
+MAPS.calculateDistance = function(place, marker) {
+
+        //POINT RETURNS ACTUAL LAT AND LNG OF THE PLACE TO HELP CALCULATING THE DISTANCE
+        point = function(){
+            var lat = place.geometry.location.lat(),
+                lng = place.geometry.location.lng();
+            return {
+                lat: lat,
+                lng: lng
+            };
+        };
+
+        //GET DISTANCE BETWEEN origins AND destinations WITH 'WALKING' MODE
+        this.distance.getDistanceMatrix({
+            origins: [this.cords],
+            destinations: [point()],
+            travelMode: 'WALKING'
+        }, function(response,status){
+
+            //get duration value (in miutes)
+            var duration = response.rows[0].elements[0].duration.text;
+
+            //CALL THE DISPLAY LIST function
+            this.displayList(duration, place, marker);
+        }.bind(this));
+};
+
+
+//THIS FUNCTION WILL DISPLAY THE LIST OF PLACES
+//IT TAKSE 3 ARGUMENTS: duration - TRAVEL TIME IN MINUTES, place, AND marker - TO LINK LIST WITH MARKER ON THE MAP
+MAPS.displayList = function(duration, place, marker){
+
+    //CREATE DIV THAT WILL BE CONTAINER FOR EACH PLACE ITEM ON THE LIST
+    var div = document.createElement('div');
+    div.classList.add('map-list__item');
+
+    var span = '<span>'+place.name+', duration: '+duration+'</span>';
+    div.innerHTML = span;
+    this.placesList.appendChild(div);
+
+
+    //ADD EVENT LISTENERS WHEN INTERACTING WITH THE LIST
+    div.addEventListener('mouseover',function(){
+        marker.setIcon('');
+    },false);
+
+    div.addEventListener('mouseleave',function(){
+        marker.setIcon('https://developers.google.com/maps/documentation/javascript/images/circle.png');
+    },false);
+
+    div.addEventListener('click',function(){
+        this.service.getDetails(place, function(result, status){
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+            }
+            div.innerHTML += "DUPA";
+        }.bind(this));
+    }.bind(this),false);
+
+
+};
+
+
+//DOMHANDLER
+
+var DOMHANDLER = {
+    setType: function(type) {
+        this.type = type;
+    },
+    getType: function() {
+        return this.type;
+    },
+    setSort: function(sort) {
+        this.sort = sort;
+    },
+    getSort: function() {
+        return this.sort;
+    }
+};
+
+
+DOMHANDLER.init = function(){
+    var inputType = document.getElementById('input-type'),
+        inputSort = document.getElementById('input-sort');
+        this.setType(inputType.value);
+        this.setSort(inputSort.value);
+
+        inputType.addEventListener('change', this.clickListener.bind(this), false);
+        inputSort.addEventListener('change', this.clickListener.bind(this), false);
+}
+
+
+DOMHANDLER.clickListener = function(e) {
+    var src = e.src || e.target,
+        value = src.value,
+        config = {};
+
+    if (src.id === 'input-type') {
+        this.setType(value);
+    }
+    if (src.id === 'input-sort') {
+        this.setSort(value);
+    }
+    config = {
+        type: this.getType(),
+        sort: this.getSort()
+    }
+
+    MAPS.initialize(config);
+
+};
+
+DOMHANDLER.init();
+MAPS.initialize({
+    type: document.getElementById('input-type').value,
+    sort: document.getElementById('input-sort')
+});
