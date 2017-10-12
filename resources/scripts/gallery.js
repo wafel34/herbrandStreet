ba39506ccb94c5398e1cbed0130bfd9d783c8d99
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-10-12 13:59:41 +0100

    Added simple scrolling and continue to work on hallery

--- resources/scripts/gallery.js
+++ resources/scripts/gallery.js
@@ -3,8 +3,30 @@ var GALLERY = {
 };
 
 GALLERY.collectImages = function() {
-    var collection = document.querySelectorAll('.gallery__item');
+    var collection = document.querySelectorAll('.gallery__item'),
+        arr = Array.prototype.slice.call( collection ),
+        div = document.createElement('div'),
+        content = '';
 
+    div.classList.add('full-screen-gallery');
+    div.classList.add('full-screen-gallery--hidden');
+
+    arr.map(function(item, index){
+        var id = index+1;
+        console.log(item);
+        content +=
+            '<img class="full-screen-gallery__image" src="./images/'+id+'.jpg" alt="">';
+
+        item.addEventListener('click', function(e){
+                div.classList.toggle('full-screen-gallery--hidden');
+                div.classList.toggle('full-screen-gallery--visible');
+        },false);
+
+    });
+
+
+    div.innerHTML = content;
+    document.body.appendChild(div);
 
 };
 
---
+++ resources/scripts/scroll.js
@@ -0,0 +1,6 @@
+(function(){
+    var SmoothScroll = require('smooth-scroll');
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        easing: 'easeInOutQuad'
+    });
+}());
