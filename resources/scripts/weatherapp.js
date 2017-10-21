3bd72994a2ac765877e58c600bd809d497612311
Pawel Maricnkowski <wafel34@gmail.com>
2017-10-21 14:21:53 +0100

    Improving weather app

--- resources/scripts/weatherapp.js
+++ resources/scripts/weatherapp.js
@@ -5,14 +5,14 @@ var WEATHERAPI = {
 WEATHERAPI.init = function() {
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=a472ebd642da3e64e35da328aaf3531c&units=metric',true);
+    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=a472ebd642da3e64e35da328aaf3531c&units=metric',true);
 
     xhr.onreadystatechange = function(data) {
         if (xhr.readyState !== 4 ) {
             return false;
         }
         if (xhr.status !== 200 ){
-            console.err('Status error: ' + xhr.status);
+            console.log('Status error: ' + xhr.status);
         }
 
         var results = {},
